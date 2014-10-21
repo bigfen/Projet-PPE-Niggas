e265ffb6e113fcd43239fecfa58f7eaf29c07494
bigfen <justagasy@gmail.com>
2014-10-21 17:31:09 +0200

    bar nav

---
+++ barnav.php
@@ -0,0 +1,12 @@
+<nav>     
+	   <table>
+	   	<th><a href="actualites.php">Musa Paradisiaca</a></th>
+		<th><a href="famille.php">Cavendish</a></th>
+		<th><a href="cruclasse.php">Poyo</a></th>
+		<th><a href="selection.php">Banane Dessert Exotique</a></th>
+		<th><a href="vins_et_millesimes.php">Figue Pomme</a></th>
+		<th><a href="accordsmetsvins.php">Banane Rose</a></th>
+		<th><a href="presse.php">Pisang lilin</a></th>
+		<th ><a href="contact.php">Mysore</a></th>
+	   </table>    
+</nav>
